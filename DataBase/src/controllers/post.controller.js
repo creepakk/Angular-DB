f7423f2c7b4e4a9dec13464c559ff72174fd0276
creepakk <daniil.shevchenko542733@gmail.com>
2023-02-01 21:31:18 +0200

    v4.1

--- DataBase/src/controllers/post.controller.js
+++ DataBase/src/controllers/post.controller.js
@@ -1,33 +1,16 @@
 const { Post } = require('../models')
-class PostController {
-    //     async createPost(req, res) {
-    //         const userId = req.params.id
-    //         const { title, content } = req.body
-    //         const newPost = await db.query('INSERT INTO post (title, content, user_id) VALUES ($1, $2, $3) RETURNING *', [title, content, userId])
-    //         res.json(newPost.rows[0])
-    //     }
 
+class PostController {
     async createPost(req, res) {
         const { title, content } = req.body
         const post = await Post.create({ title, content, user_id: req.params.id })
         res.json(post)
     }
-    //     async getPost(req, res) {
-    //         const id = req.params.id
-    //         const post = await db.query('SELECT * FROM post WHERE id = $1', [id])
-    //         res.json(post.rows[0])
-    //     }
 
     async getPost(req, res) {
         const post = await Post.findByPk(req.params.id)
         res.send(post)
     }
-    //     async updatePost(req, res) {
-    //         const id = req.params.id
-    //         const { title, content } = req.body
-    //         const post = await db.query('UPDATE post set title = $1, content = $2 WHERE id = $3 RETURNING *', [title, content, id])
-    //         res.json(post.rows[0])
-    //     }
 
     async updatePost(req, res) {
         const { title, content } = req.body
@@ -37,11 +20,6 @@ class PostController {
         })
         res.json(post[1][0])
     }
-    //     async deletePost(req, res) {
-    //         const id = req.params.id
-    //         const post = await db.query('DELETE FROM post WHERE id = $1', [id])
-    //         res.json(post.rows[0])
-    //     }
 
     async deletePost(req, res) {
         await Post.destroy({
@@ -50,20 +28,10 @@ class PostController {
         res.json()
     }
 
-    //     async getPostsByUser(req, res) {
-    //         const id = req.params.id
-    //         const posts = await db.query('SELECT * FROM post WHERE user_id = $1', [id])
-    //         res.json(posts.rows)
-    //     }
-
     async getPostsByUser(req, res) {
         const posts = await Post.findAll({ where: { user_id: req.params.id } })
         res.json(posts)
     }
-    //     async getPosts(req, res) {
-    //         const posts = await db.query('SELECT * FROM post')
-    //         res.json(posts.rows)
-    //     }
 
     async getPosts(req, res) {
         const posts = await Post.findAll()
--- DataBase/src/models.js
+++ DataBase/src/models.js
@@ -1,16 +1,6 @@
 const { Sequelize, Model, DataTypes } = require('sequelize')
 const sequelize = new Sequelize('postgres_db', 'admin', 'admin', {
-
-    // user: "admin",
-    // password: "admin",
-    // host: "127.0.0.1",
-    // port: 5432,
-    // database: "postgres_db"
-
-    // username: 'admin',
-    // password: 'admin',
     host: 'localhost',
-    // port: 5432,
     dialect: 'postgres',
 })
 
